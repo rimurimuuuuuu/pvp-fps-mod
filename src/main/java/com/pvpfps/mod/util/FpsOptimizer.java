0d84a155ce0f1c651b09b069ace0097008e2ce91
rimurimuuuuuu <youmil8150@gmail.com>
2026-05-18 14:46:19 +0900

    Update FpsOptimizer.java

--- src/main/java/com/pvpfps/mod/util/FpsOptimizer.java
+++ src/main/java/com/pvpfps/mod/util/FpsOptimizer.java
@@ -3,13 +3,11 @@ package com.pvpfps.mod.util;
 import com.pvpfps.mod.PvpFpsMod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.option.GraphicsMode;
 import net.minecraft.client.option.ParticlesMode;
 import net.minecraft.sound.SoundCategory;
 
 /**
  * PvP FPS Boost - Core Optimizer
- * Applies all FPS-maximizing settings at runtime
  */
 public class FpsOptimizer {
 
@@ -21,7 +19,7 @@ public class FpsOptimizer {
         long maxMemory = runtime.maxMemory() / 1024 / 1024;
         PvpFpsMod.LOGGER.info("[PvP FPS Boost] Max memory: {}MB", maxMemory);
         if (maxMemory < 2048) {
-            PvpFpsMod.LOGGER.warn("[PvP FPS Boost] Low memory detected! Recommend allocating 2-4GB.");
+            PvpFpsMod.LOGGER.warn("[PvP FPS Boost] Low memory detected! Recommend 2-4GB.");
         }
     }
 
@@ -31,17 +29,9 @@ public class FpsOptimizer {
         GameOptions opts = client.options;
         var config = PvpFpsMod.CONFIG;
 
-        // Graphics mode - Fast
-        opts.graphicsMode.setValue(GraphicsMode.FAST);
-
         // Particles
         if (config.disableAllParticles) {
             opts.getParticles().setValue(ParticlesMode.MINIMAL);
-        }
-
-        // Clouds - 1.21.1 uses cloudRenderMode directly
-        if (config.disableClouds) {
-            opts.cloudRenderMode.setValue(net.minecraft.client.option.CloudRenderMode.OFF);
         }
 
         // View bobbing
@@ -59,10 +49,7 @@ public class FpsOptimizer {
         // Max FPS uncapped
         opts.getMaxFps().setValue(260);
 
-        // Smooth lighting - 1.21.1 uses smoothLighting directly
-        opts.smoothLighting.setValue(true);
-
-        // Biome blend
+        // Biome blend off
         opts.getBiomeBlendRadius().setValue(0);
 
         // Sound
